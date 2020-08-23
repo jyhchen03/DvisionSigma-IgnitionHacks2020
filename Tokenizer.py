2e1f48aa5687c43f1602540a52f769b7170ae9a4
Johnny <jyhchen03@gmail.com>
2020-08-23 17:28:40 -0600

    add pandas and dataframe

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 import csv
 import os
 import string
@@ -8,8 +10,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 from collections import defaultdict
 
+
 columns = defaultdict(list)
 re_list = [r"(?:#|@)[^\s]+",r"&.+?;",r"(?:http|www.)\S+",r"[^<3]\d+",r"[^\w\d()'<|@:;\s]+"]
+
+filename = 'C:\\Users\\Johnn\\Documents\\GitHub\\IgnitionHacks2020\\training_data.csv'
+tokenized_file = 'C:\\Users\\Johnn\\Documents\\GitHub\\IgnitionHacks2020\\tokenized_data.txt'
+dataframe = 'C:\\Users\\Johnn\\Documents\\GitHub\\IgnitionHacks2020\\dataframe.txt'
 
 def cleanup(clean_data):
     clean_data = re.sub(r"&lt;", "<", clean_data)
@@ -20,22 +27,36 @@ def cleanup(clean_data):
 def tokenize(tokenized_file, data):         #TODO: possible stopwords
     clean_data = cleanup(data)
     tknzr = TweetTokenizer(reduce_len=True)
-    tokentext = str(tknzr.tokenize(clean_data))
+    tokentext = tknzr.tokenize(clean_data)
     #print (str(tokentext) + '\n')
-    with open(tokenized_file, 'a') as f:
-        f.write(tokentext + '\n')
+    #with open(tokenized_file, 'a') as f:
+    #    f.write(tokentext + '\n')
+    return tokentext
 
-def get_raw_data(filename, tokenized_file):
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            for (k,v) in row.items():
-                columns[k].append(v) 
-    val = columns['Text']
-    for i in range(len(val)):
-        #print (val[i])
-        tokenize(tokenized_file, val[i])
+def get_tokenized_data(tokenized_file, data):
+    data_list=[]
+    for i in range(len(data)):
+        #print (data[i])
+        data_list.append(tokenize(tokenized_file, data[i]))
+    return data_list
 
-filename = 'C:\\Users\\Johnn\\Documents\\GitHub\\IgnitionHacks2020\\training_data.csv'
-tokenized_file = 'C:\\Users\\Johnn\\Documents\\GitHub\\IgnitionHacks2020\\tokenized_data.txt'
-get_raw_data(filename, tokenized_file)
+def get_label_column(data):
+    label_list=[]
+    x = lambda a: label_list.append(int(a))
+    for i in range(len(data)):
+        x(data[i])
+    return label_list
+
+with open(filename) as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        for (k,v) in row.items():
+            columns[k].append(v)
+
+text = get_tokenized_data(tokenized_file, columns['Text'])
+label = get_label_column(columns['Sentiment'])
+
+pd.set_option("display.max_rows", None)
+dataframe_list = pd.DataFrame(np.column_stack([label,text]),columns=['Sentiment','Text'])
+with open(dataframe,'a') as frame:
+    frame.write(str(dataframe_list))
